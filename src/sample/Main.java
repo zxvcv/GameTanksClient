b74da0cfa383e4669d576c2a2768d9556d1fb2a5
zxvcv <ppiskorz0@gmail.com>
2019-12-03 12:04:20 +0100

    v0.0.2  -test JavaFX program

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -1,19 +1,55 @@
 package sample;
 
+import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Group;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+    public void start(Stage mainStage)
+    {
+        mainStage.setTitle( "Timeline Example" );
+
+        Group root = new Group();
+        Scene theScene = new Scene( root );
+        mainStage.setScene( theScene );
+
+        Canvas canvas = new Canvas( 512, 512 );
+        root.getChildren().add( canvas );
+
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+
+        Image earth = new Image( "file:earth.png" );
+        Image sun   = new Image( "file:sun.png" );
+        Image space = new Image( "file:space.png" );
+
+        final long startNanoTime = System.nanoTime();
+
+        new AnimationTimer()
+        {
+            public void handle(long currentNanoTime)
+            {
+                double t = (currentNanoTime - startNanoTime) / 1000000000.0;
+
+                double x = 232 + 128 * Math.cos(t);
+                double y = 232 + 128 * Math.sin(t);
+
+                // background image clears canvas
+                gc.drawImage( space, 0, 0 );
+                gc.drawImage( earth, x, y );
+                gc.drawImage( sun, 196, 196 );
+            }
+        }.start();
+
+        mainStage.show();
     }
 
 
