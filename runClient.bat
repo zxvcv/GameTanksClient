c89d188049d6717ca48839774d1e40e0b175499b
zxvcv <ppiskorz0@gmail.com>
2019-12-16 12:30:31 +0100

    .bat runner v1.0.5  -zmieniony skrypt bat dzialajacy na obydwu moich komputerach  -gra w wiele osob dziala  -(?) po dolaczeniu drugiego gracza w pierwszym graczu jest    strasznie duzo obiektow

--- runClient.bat
+++ runClient.bat
@@ -1,9 +1,18 @@
-java^ 
-	--module-path C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib^
-	--add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics^ 
-	-classpath C:\Users\ppisk\Projects\GameTanksClient\out\production\GameTanksClient;^ 
-	sample.Main
+@echo off
 
+IF EXIST C:\Users\zxvcv\ (
+    java --module-path C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib^
+ --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics^
+ -classpath C:\Users\zxvcv\IdeaProjects\GameTanksClient\out\production\GameTanksClient^
+ sample.Main
+) ELSE (
+    java --module-path C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib^
+ --add-modules javafx.controls,javafx.fxml,javafx.base,javafx.graphics^
+ -classpath C:\Users\ppisk\Projects\GameTanksClient\out\production\GameTanksClient^
+ sample.Main
+)
+	
+set /p id="STOP"
 
 REM  --module-path C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib --add-modules javafx.controls,javafx.fxml --add-modules javafx.base,javafx.graphics --add-reads javafx.base=ALL-UNNAMED --add-reads javafx.graphics=ALL-UNNAMED "-javaagent:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.2.3\lib\idea_rt.jar=54923:C:\Program Files\JetBrains\IntelliJ IDEA Community Edition 2019.2.3\bin" -Dfile.encoding=UTF-8 -classpath C:\Users\ppisk\Projects\GameTanksClient\out\production\GameTanksClient;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\src.zip;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\javafx-swt.jar;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\javafx.web.jar;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\javafx.base.jar;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\javafx.fxml.jar;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\javafx.media.jar;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\javafx.swing.jar;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\javafx.controls.jar;C:\Libraries\openjfx-13.0.1_windows-x64_bin-sdk\javafx-sdk-13.0.1\lib\javafx.graphics.jar sample.Main
 
